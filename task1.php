bab3f67b0d73308c7125db1fc58bcab630c2c5a0
PolinaAbr <abr300793@gmail.com>
2018-05-04 13:28:37 +0300

    first

--- task1.php
+++ task1.php
@@ -1,15 +1,16 @@
 <?php
 
 $number = rand(1, 100000);
-$count = countNum($number);
+$compare = 5;
+$count = countNum($number, $compare);
 echo "Количество цифр меньше 5 в числе ".$number.": ".$count;
 
-function countNum($number) {
+function countNum($number, $compare) {
     $count = 0;
     while ($number > 0) {
         $numeral = $number % 10; //находим остаток от деления на 10 и записываем в переменную
         $number = floor ($number / 10); //убираем последнюю цифру с числа
-        if ($numeral < 5) {
+        if ($numeral < $compare) {
             $count++;
         }
     }
