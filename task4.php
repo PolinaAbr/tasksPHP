99fe75ad5d44ef6e8c9ea7e019d7e19d53f821e3
PolinaAbr <abr300793@gmail.com>
2018-05-04 15:18:45 +0300

    first

---
+++ task4.php
@@ -0,0 +1,29 @@
+<?php
+
+for ($i = 1000; $i < 10000; $i++) {
+    $increase = true; //флаг для возрастающей последовательности
+    $decrease = true; //флаг для убывающей последовательности
+    $number = $i;
+    $nextNum = $number % 10;
+    $number = floor ($number / 10);
+    while ($number > 0) {
+        $numeral = $number % 10;
+        $number = floor ($number / 10);
+        if ($numeral < $nextNum && $increase) {
+            $increase = true;
+            $decrease = false;
+            $nextNum = $numeral;
+        } elseif ($numeral > $nextNum && $decrease) {
+            $decrease = true;
+            $increase = false;
+            $nextNum = $numeral;
+        } else {
+            $increase = false;
+            $decrease = false;
+            break;
+        }
+    }
+    if ($increase || $decrease) {
+        echo $i."<br>";
+    }
+}
