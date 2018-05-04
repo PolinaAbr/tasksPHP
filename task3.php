a929737383261a18e5e3f46e6bc44bf248d9b8ef
PolinaAbr <abr300793@gmail.com>
2018-05-04 14:18:12 +0300

    commit

--- task3.php
+++ task3.php
@@ -1,1 +1,21 @@
 <?php
+$number = rand(10, 1000);
+$startNum = $number;
+$prevNum = 10;
+$result = false;
+while ($number > 0) {
+    $numeral = $number % 10;
+    $number = floor ($number / 10);
+    if ($numeral < $prevNum) {
+        $result = true;
+        $prevNum = $numeral;
+    } else {
+        $result = false;
+        break;
+    }
+}
+if ($result) {
+    echo "Цифры числа ".$startNum." образуют возрастающую последовательность";
+} else {
+    echo "Цифры числа ".$startNum." не образуют возрастающую последовательность";
+}
