a37c474012182b1af21702d9b9a782b4de3d6d4d
PolinaAbr <abr300793@gmail.com>
2018-05-04 16:15:27 +0300

    first commit

--- task7.php
+++ task7.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Polina
- * Date: 04.05.2018
- * Time: 13:22
- */
+
+$number = rand(100, 1000);
+$startNum = $number;
+$result = false;
+while ($number > 0) {
+    $numeral = $number % 10;
+    $number = floor($number / 10);
+    $newNum = $number;
+    while ($newNum > 0) {
+        $numeral2 = $newNum % 10;
+        $newNum = floor($newNum / 10);
+        if ($numeral === $numeral2) {
+            $result = true;
+        }
+    }
+}
+if ($result) {
+    echo "В числе ".$startNum." есть повторяющиеся цифры";
+} else {
+    echo "В числе ".$startNum." нет повторяющтхся цифр";
+}
