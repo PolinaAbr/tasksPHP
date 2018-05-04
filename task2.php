18a685b12d6d242d5253c7cad69398912d489972
PolinaAbr <abr300793@gmail.com>
2018-05-04 12:53:03 +0300

    first commit

---
+++ task2.php
@@ -0,0 +1,14 @@
+<?php
+$numeralSum = rand(1, 36);
+echo "Числа, сумма цифр которых равна ".$numeralSum."<br>";
+for ($i = 1000; $i < 1101; $i++) {
+    $sum = 0;
+    $number = $i;
+    for ($j = 0; $j < 4; $j++) {
+        $sum += $number % 10;
+        $number = floor ($number / 10);
+    }
+    if ($numeralSum === $sum) {
+        echo $i."<br>";
+    }
+}
