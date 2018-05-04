8828cf3715e28483124bdaed8093a5f6c9420582
PolinaAbr <abr300793@gmail.com>
2018-05-04 15:54:39 +0300

    first commit

--- task6.php
+++ task6.php
@@ -1,7 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Polina
- * Date: 04.05.2018
- * Time: 13:21
- */
+
+$num1 = 0;
+$num2 = 2;
+$num3 = 3;
+$num4 = 7;
+for ($i = 1000; $i < 10000; $i++) {
+    $result = false;
+    $number = $i;
+    while ($number > 0) {
+        $numeral = $number % 10;
+        $number = floor($number / 10);
+        if ($numeral === $num1 || $numeral === $num2 || $numeral === $num3 || $numeral === $num4) {
+            $result = true;
+        } else {
+            $result = false;
+            break;
+        }
+    }
+    if ($result) {
+        echo $i."<br>";
+    }
+}
