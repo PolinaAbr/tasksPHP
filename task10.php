a9c9d865ed804b7f16c3af7bb32d5d0f1483a9ad
PolinaAbr <abr300793@gmail.com>
2018-05-07 10:02:40 +0300

    first commit

--- task10.php
+++ task10.php
@@ -1,7 +1,43 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Polina
- * Date: 04.05.2018
- * Time: 13:24
- */
+
+$maxNumber = 100;
+$resultNum = false;
+$resultSquare = false;
+for ($number = 1; $number < $maxNumber; $number++) {
+    $reverseNum = reverseNumber($number);
+    $resultNum = palindrome($number, $reverseNum);
+    if ($resultNum) {
+        $square = $number * $number;
+        $reverseSquare = reverseNumber($square);
+        $resultSquare = palindrome($square, $reverseSquare);
+    }
+    if ($resultNum && $resultSquare) {
+        echo $number."<br>";
+    }
+}
+
+function reverseNumber($number) {
+    $reverseNum = 0;
+    while ($number > 0) {
+        $numeral = $number % 10;
+        $number = floor($number / 10);
+        $reverseNum = $reverseNum * 10 + $numeral;
+    }
+    return $reverseNum;
+}
+
+function palindrome($number, $reverseNum) {
+    $result = false;
+    while ($number > 0) {
+        $numeral = $number % 10;
+        $number = floor($number / 10);
+        $numeralReverse = $reverseNum % 10;
+        $reverseNum = floor($reverseNum / 10);
+        if ($numeral === $numeralReverse) {
+            $result = true;
+        } else {
+            $result = false;
+        }
+    }
+    return $result;
+}
