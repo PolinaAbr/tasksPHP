7ed449462c1e2908db963e37e51930504b96f743
PolinaAbr <abr300793@gmail.com>
2018-05-08 11:21:20 +0300

    first

--- arrTask14.php
+++ arrTask14.php
@@ -1,7 +1,30 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Polina
- * Date: 08.05.2018
- * Time: 10:10
- */
+
+$array = array(array(0, 1, 0, 0, 1), array(0, 0, 0, 0, 0), array(0, 1, 0, 0, 1), array(0, 0, 0, 0, 0));
+$columns = 5;
+$rows = 4;
+for ($i = 0; $i < $rows; $i++) {
+    for ($j = 0; $j < $columns; $j++) {
+        if ($j == $columns - 1) {
+            echo $array[$i][$j]."<br>";
+        } else {
+            echo $array[$i][$j]." ";
+        }
+    }
+}
+
+$nullRow = false;
+for ($i = 0; $i < $rows; $i++) {
+    for ($j = 0; $j < $columns; $j++) {
+        if ($array[$i][$j] == 0) {
+            $nullRow = true;
+        } else {
+            $nullRow = false;
+            break;
+        }
+    }
+    if ($nullRow) {
+        unset($array[$i]);
+    }
+}
+print_r($array);
