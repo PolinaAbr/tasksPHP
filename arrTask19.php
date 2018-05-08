064af6a0faae62713a4b55abeb12d9dbd17d2bcf
PolinaAbr <abr300793@gmail.com>
2018-05-08 15:03:07 +0300

    first

--- arrTask19.php
+++ arrTask19.php
@@ -1,7 +1,59 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Polina
- * Date: 08.05.2018
- * Time: 13:34
- */
+
+$array = array();
+$size = 6;
+for ($i = 0; $i < $size; $i++) {
+    for ($j = 0; $j < $size; $j++) {
+        $array[$i][$j] = rand(10, 99);
+        if ($j == $size - 1) {
+            echo $array[$i][$j]."<br>";
+        } else {
+            echo $array[$i][$j]." ";
+        }
+    }
+}
+
+$newArray = array();
+$middle = $size / 2;
+if ($size % 2 !==0) {
+    for ($i = 0; $i < $size; $i++) {
+        for ($j = 0; $j < $size; $j++) {
+            if ($i < floor($middle) && $j < floor($middle)) {
+                $newArray[$i + ceil($middle)][$j + ceil($middle)] = $array[$i][$j];
+            } elseif ($i < floor($middle) && $j > floor($middle)) {
+                $newArray[$i + ceil($middle)][$j - ceil($middle)] = $array[$i][$j];
+            } elseif ($i > floor($middle) && $j < floor($middle)) {
+                $newArray[$i - ceil($middle)][$j + ceil($middle)] = $array[$i][$j];
+            } elseif ($i > floor($middle) && $j > floor($middle)) {
+                $newArray[$i - ceil($middle)][$j - ceil($middle)] = $array[$i][$j];
+            } else {
+                $newArray[$i][$j] = $array[$i][$j];
+            }
+        }
+    }
+} else {
+    for ($i = 0; $i < $size; $i++) {
+        for ($j = 0; $j < $size; $j++) {
+            if ($i < $middle && $j < $middle) {
+                $newArray[$i + $middle][$j + $middle] = $array[$i][$j];
+            } elseif ($i < $middle && $j >= $middle) {
+                $newArray[$i + $middle][$j - $middle] = $array[$i][$j];
+            } elseif ($i >= $middle && $j < $middle) {
+                $newArray[$i - $middle][$j + $middle] = $array[$i][$j];
+            } elseif ($i >= $middle && $j >= $middle) {
+                $newArray[$i - $middle][$j - $middle] = $array[$i][$j];
+            }
+        }
+    }
+}
+
+echo "<br>Новый массив:<br>";
+for ($i = 0; $i < $size; $i++) {
+    for ($j = 0; $j < $size; $j++) {
+        if ($j == $size - 1) {
+            echo $newArray[$i][$j] . "<br>";
+        } else {
+            echo $newArray[$i][$j] . " ";
+        }
+    }
+}
