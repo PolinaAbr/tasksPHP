85c53f593069e7d08fbba434b4128154b475df1f
PolinaAbr <abr300793@gmail.com>
2018-05-13 23:13:55 +0300

    first

---
+++ combinations.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Polina
+ * Date: 10.05.2018
+ * Time: 15:53
+ */
---
+++ index.php
@@ -0,0 +1,106 @@
+<?php
+
+$string = "012";
+$length = 3;
+//вычисляем количество возможных комбинаций
+$count = factorial(strlen($string)) / factorial(strlen($string) - $length);
+
+$array = array();
+$max = strlen($string) - 1;
+$row = 1;
+
+//заполняем первую строку массива индексами первой комбинации
+for ($i = 0; $i < $length; $i++) {
+    $array[0][$i] = $i;
+}
+
+//пока массив не будет заполнен числом строк $count
+while (count($array) < $count) {
+    //указатель на последнем элементе комбинации
+    $position = $length - 1;
+    $array[$row] = combination($array[$row - 1], $position, $max);
+    $row++;
+}
+
+//вывод всех полученных комбинаций
+for ($i = 0; $i < $count; $i++) {
+    echo $i + 1;
+    echo ". ";
+    for ($j = 0; $j < $length; $j++) {
+        echo $string[$array[$i][$j]];
+    }
+    echo "<br>";
+}
+
+function combination($prev, $position, $max) {
+    if ($prev[$position] + 1 > $max) {
+        $position--;
+    }
+    if ($position == 0) {
+        $new[$position] = $prev[$position] + 1;
+    } else {
+        $a = 1;
+        $new = unique($prev, $position, $a, $max);
+    }
+    //цикл для проверки на уникадьность значения
+    for ($i = $position + 1; $i < count($prev); $i++) {
+        //начинаем проверку с наименьшего значения
+        $number = 0;
+        $new[$i] = after_position($i, $new, $number);
+    }
+    return $new;
+}
+
+function unique($prev, $position, $a, $max) {
+    $result = false;
+    $new = array();
+    for ($j = 0; $j < $position; $j++) {
+        $new[$j] = $prev[$j];
+    }
+    if ($prev[$position] + $a <= $max) {
+        //цикл для проверки на уникальность значения
+        for ($j = 0; $j < $position; $j++) {
+            if ($prev[$position] + $a !== $new[$j]) {
+                $result = true;
+            } else {
+                $a++;
+                return unique($prev, $position, $a, $max);
+            }
+        }
+        if ($result) {
+            $new[$position] = $prev[$position] + $a;
+            return $new;
+        }
+    } else {
+        $position--;
+        return combination($prev, $position, $max);
+    }
+}
+
+//заполнение массива после установленной позиции
+function after_position($i, $array, $number) {
+    $result = false;
+    for ($j = 0; $j < $i; $j++) {
+        //если число не совпадает с элементом массива
+        if ($number !== $array[$j]) {
+            $result = true;
+        } else {
+            $result = false;
+            break;
+        }
+    }
+    if ($result) {
+        return $number;
+    } else {
+        return after_position($i, $array, $number + 1);
+    }
+}
+
+//функция нахождения факториала числа
+function factorial($number){
+    if ($number === 0) {
+        return 1;
+    } else {
+        return $number * factorial($number - 1);
+    }
+}
