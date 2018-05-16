e051861199ed71eed27504e0e5cf974f5e67a800
PolinaAbr <abr300793@gmail.com>
2018-05-16 17:14:15 +0300

    first

--- arrTask11.php
+++ arrTask11.php
@@ -1,7 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Polina
- * Date: 08.05.2018
- * Time: 10:10
- */
+
+$array = array();
+$columns = 4;
+$rows = 3;
+for ($i = 0; $i < $rows; $i++) {
+    for ($j = 0; $j < $columns; $j++) {
+        $array[$i][$j] = rand(-99, 99);
+        if ($j == $columns - 1) {
+            echo $array[$i][$j]."<br>";
+        } else {
+            echo $array[$i][$j]." ";
+        }
+    }
+}
+
+echo "<br>Новый массив:<br>";
+for ($i = 0; $i < $rows; $i++) {
+    for ($j = 0; $j < $columns; $j++) {
+        if ($array[$i][$j] > 0 && $array[$i][$j] % 2 == 0) {
+            unset($array[$i][$j]);
+        } else {
+            echo $array[$i][$j]." ";
+        }
+    }
+    echo "<br>";
+}
