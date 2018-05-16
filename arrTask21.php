bc9b28e9880cbf5d0ee39b6c0cfed25dc56d3407
PolinaAbr <abr300793@gmail.com>
2018-05-16 17:32:52 +0300

    first

--- arrTask21.php
+++ arrTask21.php
@@ -1,7 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Polina
- * Date: 08.05.2018
- * Time: 13:51
- */
+
+$array = array();
+$columns = 7;
+$rows = 5;
+for ($i = 0; $i < $rows; $i++) {
+    for ($j = 0; $j < $columns; $j++) {
+        $array[$i][$j] = rand(0, 9);
+        echo $array[$i][$j]." ";
+    }
+    echo "<br>";
+}
+
+for ($i = 0; $i < $rows; $i++) {
+    sort($array[$i]);
+}
+
+echo "<br>Новый массив<br>";
+for ($i = 0; $i < $rows; $i++) {
+    for ($j = 0; $j < $columns; $j++) {
+        echo $array[$i][$j]." ";
+    }
+    echo "<br>";
+}
+
+
+function mySort($array) {
+    for ($i = 0; $i < count($array) - 1; $i++){
+        for ($j = 0; $j < count($array) - $i - 1; $j++){
+            if ($array[$j] > $array[$j + 1]){
+                $temp = $array[$j + 1];
+                $array[$j + 1] = $array[$j];
+                $array[$j] = $temp;
+            }
+        }
+    }
+}
