d7a38ba537878ab346e02fc5be3e47dc7503660b
PolinaAbr <abr300793@gmail.com>
2018-05-17 13:32:34 +0300

    first

--- arrTask12.php
+++ arrTask12.php
@@ -6,18 +6,16 @@ $rows = 5;
 for ($i = 0; $i < $rows; $i++) {
     for ($j = 0; $j < $columns; $j++) {
         $array[$i][$j] = rand(-99, 99);
-        if ($j == $columns - 1) {
-            echo $array[$i][$j]."<br>";
-        } else {
-            echo $array[$i][$j]." ";
-        }
+        echo $array[$i][$j]." ";
     }
+    echo "<br>";
 }
 
-for ($i = 0; $i < $rows; $i++) {
+$array = transpMatrix($array, $columns, $rows);
+for ($i = 0; $i < $columns; $i++) {
     $minus = array();
     $min = 99;
-    for ($j = 0; $j < $columns; $j++) {
+    for ($j = 0; $j < $rows; $j++) {
         if ($array[$i][$j] < 0) {
             $minus[] = $j;
         }
@@ -26,19 +24,77 @@ for ($i = 0; $i < $rows; $i++) {
         }
     }
     if ($minus) {
-        array_splice($array[$i], $minus[0] + 1, 0, $min);
+        $array[$i] = insertElement($array[$i], $minus[0] + 1, $min);
     } else {
-        array_splice($array[$i], 0, 0, $min);
+        $array[$i] = insertElement($array[$i], 0, $min);
     }
 }
+$array = transpMatrix($array, $rows + 1, $columns);
 
 echo "<br>Новый массив:<br>";
-for ($i = 0; $i < $rows; $i++) {
-    for ($j = 0; $j <= $columns; $j++) {
-        if ($j == $columns) {
-            echo $array[$i][$j] . "<br>";
-        } else {
-            echo $array[$i][$j] . " ";
+for ($i = 0; $i < count($array); $i++) {
+    for ($j = 0; $j < count($array[$i]); $j++) {
+        echo $array[$i][$j] . " ";
+    }
+    echo "<br>";
+}
+
+function insertElement($array, $position, $element) {
+    $new = array();
+    for ($i = 0; $i < $position; $i++) {
+        $new[$i] = $array[$i];
+    }
+    $new[$position] = $element;
+    if ($position < count($array) - 1) {
+        for ($i = $position; $i < count($array); $i++) {
+            $new[$i + 1] = $array[$i];
         }
     }
+    return $new;
+}
+
+function transpMatrix($matrix, $columns, $rows) {
+    $toSquare = false;
+    if ($columns !== $rows) {
+        $matrix = squareMatrix($matrix, $columns, $rows);
+        $toSquare = true;
+    }
+    $transp = array();
+    for ($i = 0; $i < count($matrix); $i++) {
+        for ($j = 0; $j < count($matrix); $j++) {
+            $transp[$i][$j] = $matrix[$j][$i];
+        }
+    }
+    if ($toSquare) {
+        $transp = matrix($transp);
+    }
+    return $transp;
+}
+
+function squareMatrix($matrix, $columns, $rows) {
+    if ($columns < $rows) {
+        for ($i = 0; $i < $rows; $i++) {
+            for ($j = $columns; $j < $rows; $j++) {
+                $matrix[$i][$j] = null;
+            }
+        }
+    } else {
+        for ($i = $rows; $i < $columns; $i++) {
+            for ($j = 0; $j < $columns; $j++) {
+                $matrix[$i][$j] = null;
+            }
+        }
+    }
+    return $matrix;
+}
+
+function matrix($matrix) {
+    for ($i = 0; $i < count($matrix); $i++) {
+        for ($j = 0; $j < count($matrix); $j++) {
+            if ($matrix[$i][$j] == null) {
+                unset($matrix[$i][$j]);
+            }
+        }
+    }
+    return $matrix;
 }
